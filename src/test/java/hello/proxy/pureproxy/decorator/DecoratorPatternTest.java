24ae167c527b0f9722ef3445906e7dcd38340104
LeeJeongGi <apple4rhk@naver.com>
2022-05-22 16:11:07 +0900

    feat : Decorator Pattern Code V3

--- src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,9 +1,6 @@
 package hello.proxy.pureproxy.decorator;
 
-import hello.proxy.pureproxy.decorator.code.Component;
-import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
-import hello.proxy.pureproxy.decorator.code.MessageDecorator;
-import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.*;
 import org.junit.jupiter.api.Test;
 
 public class DecoratorPatternTest {
@@ -24,6 +21,16 @@ public class DecoratorPatternTest {
         DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
 
         client.execute();
+    }
 
+    @Test
+    void decorator2() {
+
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+
+        client.execute();
     }
 }
