40df6bde9a14ad218cd72d8bc9768d2acb23b870
LeeJeongGi <apple4rhk@naver.com>
2022-06-01 20:29:43 +0900

    feat : Basic Bean 등록 방법

---
+++ src/test/java/hello/proxy/postprocessor/BasicTest.java
@@ -0,0 +1,54 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BasicTest {
+
+    @Test
+    void basicConfig() {
+
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+        //A는 빈으로 등록
+        A a = applicationContext.getBean("beanA", A.class);
+        a.helloA();
+
+        //B는 빈으로 등록 x
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> applicationContext.getBean(B.class));
+
+    }
+
+    @Slf4j
+    @Configuration
+    static class BasicConfig {
+
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+
+    }
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+    }
+}
